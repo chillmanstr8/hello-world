c8df1dac543fd3b9884bf0c1282969210f302e77
Ian <chillmanstr8@users.noreply.github.com>
2020-04-07 21:49:00 -0400

    created basic helloworld file in PoSH

---
+++ HelloWorld.ps1
@@ -0,0 +1,9 @@
+#################################################################
+#
+#       Hello World - first PowerShell file on github.. maybe
+#                 -ics 4/7/2020
+#################################################################
+
+$text = "Hello, World, you bastard!"
+
+Write-Output $text -ForegroundColor Red
